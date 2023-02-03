83e1d82a00422ee777c69affa24d346e1ae07f10
arunbingari <arunbingari@gmail.com>
2023-02-03 15:40:17 +0530

    added subnetmoduel

--- main.tf
+++ main.tf
@@ -3,15 +3,7 @@ provider "aws" {
     region = "ap-south-1"
 }
 
-variable vpc_cidr_block {}
-variable subnet_cidr_block {}
-variable avail_zone {}
-variable env_prefix {}
-variable my_ip {}
-variable instance_type {}
-variable public_key_location {}
-variable private_key_location {}
-  
+
 
 
 resource "aws_vpc" "myapp-vpc" {
@@ -21,32 +13,14 @@ resource "aws_vpc" "myapp-vpc" {
     }
 }
 
-resource "aws_subnet" "myapp-subnet-1" {
+module "myapp-subnet-1" {
+    source = "./modules/subnet"
+    subnet_cidr_block = var.subnet_cidr_block
+    avail_zone = var.avail_zone
+    env_prefix = var.env_prefix
     vpc_id = aws_vpc.myapp-vpc.id
-    cidr_block = var.subnet_cidr_block
-    availability_zone = var.avail_zone
-    tags = {
-        Name = "${var.env_prefix}-subnet-1"
-    }
-}
-
-resource "aws_internet_gateway" "myapp-igw" {
-    vpc_id = aws_vpc.myapp-vpc.id
-    tags = {
-        Name = "${var.env_prefix}-igw"
-    }
-}
-
-resource "aws_default_route_table" "main-rtb" {
     default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
 
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.myapp-igw.id
-    }
-    tags = {
-        Name = "${var.env_prefix}-main-rtb"
-    }
 }
 
 resource "aws_default_security_group" "default-sg" {
@@ -112,13 +86,6 @@ data "aws_ami" "latest-amazon-linux-image" {
     }
 }
 
-output "aws_ami_id" {
-    value = data.aws_ami.latest-amazon-linux-image.id
-}
-
-output "ec2_public_ip" {
-    value = aws_instance.myapp-server.public_ip
-}
 
 resource "aws_key_pair" "ssh-key" {
     key_name = "server-key"
@@ -129,41 +96,16 @@ resource "aws_instance" "myapp-server" {
     ami = data.aws_ami.latest-amazon-linux-image.id
     instance_type = var.instance_type
 
-    subnet_id = aws_subnet.myapp-subnet-1.id
+    subnet_id = module.myapp-subnet-1.subnet.id
     vpc_security_group_ids = [aws_default_security_group.default-sg.id]
     availability_zone = var.avail_zone
 
     associate_public_ip_address = true
     key_name = aws_key_pair.ssh-key.key_name
 
-   # kjsdc user_data = file("entry-script.sh")
-connection {
-  type = "ssh"
-  host = self.public_ip
-  user = "ec2-user"
-  private_key = file(var.private_key_location)
+   user_data = file("entry-script.sh")
 
-}
-
-provisioner "file" {
-    source = "entry-script.sh"
-    destination = "/home/ec2-user/entry-script-on-ec2.sh"
-  
-}
-
-    provisioner "remote-exec" {
-        script = file("entry-script-on-ec2.sh")
-        inline = [
-          "export ENV=dev",
-          "mkdir newdir",
-          
-        ]
-      
-    }
-    provisioner "local-exec" {
-        command = "echo ${aws_instance.myapp-server.public_ip} > output.txt"
-      
-    }
+    
     tags = {
         Name = "${var.env_prefix}-server"
     }
